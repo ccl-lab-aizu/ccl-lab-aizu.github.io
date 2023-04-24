c4a9abc57f9d840d6cb89ecee21a0bef83fd8e65
xiaocuong <cuong98hp@gmail.com>
2023-04-24 16:29:25 +0900

    refactor the site strcuture

--- jemdoc_files/make.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-./jemdoc -c mysite.conf -o ../  *.jemdoc
-
-# now=$(date) 
-# git add -A
-# git commit -m "change on $now "
-# git push
---
+++ make.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+dir=$(pwd)
+
+cd jemdoc_files
+./jemdoc -c mysite.conf -o ../ index.jemdoc
+cd $dir
+
+cd jemdoc_files/AY2022
+./jemdoc -c ../mysite.conf -o ../../html/AY2022/ *.jemdoc
+cd $dir
+
+cd jemdoc_files/AY2023
+./jemdoc -c ../mysite.conf -o ../../html/AY2023/ *.jemdoc
+cd $dir
