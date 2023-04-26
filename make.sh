5f8e3cdc75e42db96ad3afb1a7f04f50aedb77fe
xiaocuong <cuong98hp@gmail.com>
2023-04-26 16:02:28 +0900

    update Apr. 26

--- make.sh
+++ make.sh
@@ -3,13 +3,13 @@
 dir=$(pwd)
 
 cd jemdoc_files
-./jemdoc -c mysite.conf -o ../ index.jemdoc
+./jemdoc -c mainsite.conf -o ../ index.jemdoc
 cd $dir
 
 cd jemdoc_files/AY2022
-./jemdoc -c mysite.conf -o ../../html/AY2022/ *.jemdoc
+./jemdoc -c ../subsite.conf -o ../../html/AY2022/ *.jemdoc
 cd $dir
 
 cd jemdoc_files/AY2023
-./jemdoc -c mysite.conf -o ../../html/AY2023/ *.jemdoc
+./jemdoc -c ../subsite.conf -o ../../html/AY2023/ *.jemdoc
 cd $dir
