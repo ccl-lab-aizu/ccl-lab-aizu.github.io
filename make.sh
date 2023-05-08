2287e23bddd688d2b00232e0a4141b69c1c88a4c
xiaocuong <cuong98hp@gmail.com>
2023-05-08 09:32:18 +0900

    update new css and layout

--- make.sh
+++ make.sh
@@ -6,10 +6,22 @@ cd jemdoc_files
 ./jemdoc -c mainsite.conf -o ../ index.jemdoc
 cd $dir
 
-cd jemdoc_files/AY2022
-./jemdoc -c ../subsite.conf -o ../../html/AY2022/ *.jemdoc
+cd jemdoc_files/RegSeminars
+./jemdoc -c ../subsite.conf -o ../../html/RegSeminars/ *.jemdoc
 cd $dir
 
-cd jemdoc_files/AY2023
-./jemdoc -c ../subsite.conf -o ../../html/AY2023/ *.jemdoc
+cd jemdoc_files/GradSeminars
+./jemdoc -c ../subsite.conf -o ../../html/GradSeminars/ *.jemdoc
+cd $dir
+
+cd jemdoc_files/ConfRehearsals
+./jemdoc -c ../subsite.conf -o ../../html/ConfRehearsals/ *.jemdoc
+cd $dir
+
+cd jemdoc_files/Camp
+./jemdoc -c ../subsite.conf -o ../../html/Camp/ *.jemdoc
+cd $dir
+
+cd jemdoc_files/InvitedTalks
+./jemdoc -c ../subsite.conf -o ../../html/InvitedTalks/ *.jemdoc
 cd $dir
